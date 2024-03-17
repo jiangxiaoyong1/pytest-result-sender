30c461ada11cb75a87d54a4c61e7ac17b48cc148
maomao <cvf.1206@163.com>
2024-03-17 14:53:11 +0800

    完善需求

--- conftest.py
+++ conftest.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @File    : plugin.py
-# @Time    : 2024/3/16 18:15
+# @File    : conftest.py
+# @Time    : 2024/3/17 14:04
 # @Author  : 一蒋晓勇
 # @Software: PyCharm
+import time
 from datetime import datetime
 import pytest
 import logging
 
-data = {}
+data = {
+    "passed": 0,
+    "failed": 0,
+}
+
+
+def pytest_runtest_logreport(report:pytest):
+    if report.when == 'call':
+        # print("本次用例执行结果：", report.outcome)
+        data[report.outcome] += 1
 
 
 @pytest.hookimpl(tryfirst=True)
@@ -16,7 +26,7 @@ def pytest_collection_finish(session: pytest.Session) -> None:
     """用例加载完成之后，包含了全部的用例"""
     logging.info('获取用例总数')
     data['total'] = len(session.items)
-    print("用例的总数", data['total'])
+    print("用例的总数:", data['total'])
 
 
 @pytest.hookimpl(tryfirst=True)
@@ -24,6 +34,7 @@ def pytest_configure():
     # 配置加载完毕之后执行，测试用例执行前执行
     logging.info('用例执行开始时间')
     data['start_time'] = datetime.now()
+    # data['start_time'] = time.perf_counter()
     # print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}, pytest开始执行")
     print(f"{datetime.now()}, pytest开始执行")
 
@@ -39,5 +50,6 @@ def pytest_unconfigure():
     data['duration'] = data['end_time'] - data['start_time']
     logging.info("测试用例通过率")
     data['pass_ratio'] = data['passed'] / data['total'] * 100
-    data['pass_ratio'] = f"{data['pass_ratio']:.%}"
+    data['pass_ratio'] = f"{data['pass_ratio']:.2f}%"
     print(f'{data["duration"]}')
+    print(f'{data["pass_ratio"]}')
---
+++ tests/test_abc.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File    : test_abc.py
+# @Time    : 2024/3/17 01:02
+# @Author  : 一蒋晓勇
+# @Software: PyCharm
+import time
+
+
+def test_api_001():
+    time.sleep(2)
+
+
+def test_api_002():
+    time.sleep(2)
+
+
+def test_api_003():
+    time.sleep(2)
+
+
+def test_api_004():
+    time.sleep(2)
